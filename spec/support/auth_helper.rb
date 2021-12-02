f09877b1df16a933dc94f8b9bc81be198f78d29f
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-02 14:19:04 +0800

    Delete spec directory

--- spec/support/auth_helper.rb
+++
@@ -1,7 +0,0 @@
-module AuthHelper
-  def http_login
-    user = 'user'
-    pass = 'dummy-password'
-    request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(user, pass)
-  end
-end
